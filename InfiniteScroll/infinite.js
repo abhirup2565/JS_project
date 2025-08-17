b70334b723a5ba5eec79efbcd5dc8a32bcf31061
abhirup2565 <abhirup2565@gmail.com>
2025-08-17 17:25:59 +0530

    infinite scroll

---
+++ InfiniteScroll/infinite.js
@@ -0,0 +1,30 @@
+const itemContainer = document.getElementById('container');
+const sentinel = document.getElementById('sentinel');
+
+let bachSize = 25;
+let counter = 0 ;
+
+const generateItems = ()=>{
+    for(let i =0; i<bachSize;i++)
+    {
+        item = document.createElement('div');
+        item.classList.add('item');
+        item.textContent =`Item ${counter+1}`;
+        counter++;
+        itemContainer.appendChild(item);
+    }
+}
+
+window.addEventListener('load',()=>{generateItems();})
+
+const intersectionObserver  = new IntersectionObserver((entries)=>{
+    entries.forEach(entry => {
+        generateItems();
+        // if(entry.isIntersecting)
+        // {
+        //     generateItems();
+        // }
+    });
+})
+
+intersectionObserver.observe(sentinel);
